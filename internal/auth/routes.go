887c1ed0af5496caf84da57ffcb2dee7bc2ade44
Adrian PK <me@adrianpk.com>
2024-01-08 01:45:03 +0100

    Simplify routes declaration

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -1,18 +1,16 @@
 package auth
 
 func (c *WebController) routes() {
-	// TODO: Improve ergonomics of this.
-	// This will be automatically generated but still could look better.
-	c.Router().HandleFunc(c.up.ResRoute(), c.UserIndex).Methods("GET")
-	c.Router().HandleFunc(c.up.ResSlugRoute(), c.UserShow).Methods("GET")
-	c.Router().HandleFunc(c.up.ResRoute(), c.UserCreate).Methods("POST")
-	c.Router().HandleFunc(c.up.ResSlugRoute(), c.UserUpdate).Methods("PUT")
-	c.Router().HandleFunc(c.up.ResSlugRoute(), c.UserPreDelete).Methods("DELETE")
-	c.Router().HandleFunc(c.up.ResSlugRoute(), c.UserSoftDelete).Methods("PATCH")
-	c.Router().HandleFunc(c.up.ResForceDeleteRoute(), c.UserDelete).Methods("DELETE")
-	c.Router().HandleFunc(c.up.ResPurgeRoute(), c.UserPurge).Methods("DELETE")
-	c.Router().HandleFunc(c.ap.SignupRoute(), c.UserInitSignup).Methods("GET")
-	c.Router().HandleFunc(c.ap.SignupRoute(), c.UserSignup).Methods("POST")
-	c.Router().HandleFunc(c.ap.SigninPath(), c.UserInitSignin).Methods("GET")
-	c.Router().HandleFunc(c.ap.SigninPath(), c.UserSignin).Methods("POST")
+	c.Get(c.up.ResRoute(), c.UserIndex)
+	c.Get(c.up.ResSlugRoute(), c.UserShow)
+	c.Post(c.up.ResRoute(), c.UserCreate)
+	c.Put(c.up.ResSlugRoute(), c.UserUpdate)
+	c.Get(c.up.ResSlugRoute(), c.UserPreDelete)
+	c.Get(c.up.ResSlugRoute(), c.UserSoftDelete)
+	c.Delete(c.up.ResForceDeleteRoute(), c.UserDelete)
+	c.Delete(c.up.ResPurgeRoute(), c.UserPurge)
+	c.Get(c.ap.SignupRoute(), c.UserInitSignup)
+	c.Post(c.ap.SignupRoute(), c.UserSignup)
+	c.Get(c.ap.SigninPath(), c.UserInitSignin)
+	c.Post(c.ap.SigninPath(), c.UserSignin)
 }
